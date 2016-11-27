5c3d22a9521b25eefddd4cc840bbf5452fa322a6
sealband <zwzzebra@gmail.com>
2016-11-28 01:08:44 +0800

    init

---
+++ ColorBackgroundPhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColorBackgroundPhoto
+//
+//  Created by sealband on 2016/11/28.
+//  Copyright © 2016年 sealband. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
